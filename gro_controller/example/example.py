b24db6eb0ccb539ac6640a9530a2720928070505
83ca <18521057+83ca@users.noreply.github.com>
2018-10-11 12:45:25 +0900

    reformat code

--- gro_controller/example/example.py
+++ gro_controller/example/example.py
@@ -1,6 +1,5 @@
 from gro import Gro
 
-
 path = 'molecules.gro'
 gro_obj = Gro(path)
 change_res_id = list(range(1, 50, 6))
@@ -8,6 +7,3 @@ gro_obj.change_res_name('MOL2', change_res_id)
 gro_obj.delete_residues_from_name('MOL1')
 gro_obj.residue_alignment()
 gro_obj.generate_gro('new.gro')
-
-
-
